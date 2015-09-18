44316edc9bc4d05f46a2a430735bfa366cadad8e
Blake Embrey <hello@blakeembrey.com>
2015-09-17 18:09:17 -0700

    Change version for tests with deprecated version

--- src/typescript-node.ts
+++ src/typescript-node.ts
@@ -12,7 +12,7 @@ import chalk = require('chalk')
 /**
  * Export the current version.
  */
-export const VERSION = '0.1.2'
+export const VERSION = '0.1.3'
 
 /**
  * Extensions to compile using TypeScript.
